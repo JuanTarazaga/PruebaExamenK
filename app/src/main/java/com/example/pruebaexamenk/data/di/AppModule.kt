cb9a704e988867cf92f32cddb858a47f9550e2f1
Juan <juan.tarazaga.jupataxd@gmail.com>
2025-05-29 10:55:24 +0200

    .

--- app/src/main/java/com/example/pruebaexamenk/data/di/AppModule.kt
+++ app/src/main/java/com/example/pruebaexamenk/data/di/AppModule.kt
@@ -1,5 +1,6 @@
 package com.example.pruebaexamenk.data.di
 
+import com.example.pruebaexamenk.data.repositories.Repository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -9,4 +10,10 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRepository(): Repository {
+        return Repository()
+    }
 }
